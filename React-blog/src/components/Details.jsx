c5e66b423e6852e468a54d75f8596bcdb2b7cf72
Anveshna <anveshna2025@gmail.com>
2026-04-27 19:22:23 +0530

    new commit

---
+++ React-blog/src/components/Details.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import Data from '../Data'
+
+const Details = () => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const post = Data.find((item) => item.id === parseInt(id));
+    console.log(post, "deatils-post-line9");
+
+    if (!post) {
+        alert("post not found")
+    }
+
+    return (
+        <>
+        <button onClick={()=>navigate("/home")}>Back</button>
+            <div>Details</div>
+
+
+            <h1>{post.title}</h1>
+            <img src={post.img_url} alt="" />
+            <p>{post.description}</p>
+        </>
+
+    )
+}
+
+export default Details
